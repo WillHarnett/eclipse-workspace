2063b78975752878a78dac8b9203ad7e09d73424
Will Harnett <harnettwill@gmail.com>
2018-10-08 15:06:30 +0900

    Initial commit

---
+++ babbleapps-mobileautomation/src/main/java/com/babbelapps/babbleapps_mobileautomation/App.java
@@ -0,0 +1,13 @@
+package com.babbelapps.babbleapps_mobileautomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
